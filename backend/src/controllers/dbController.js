7da6400d49088d4cdbe411db5fd6e804ad353964
Nidsonr1 <nidsoncampos@hotmail.com>
2021-06-15 10:40:35 -0300

    Remove back

--- backend/src/controllers/dbController.js
+++
@@ -1,8 +0,0 @@
-const dbModels = require('../Models/dbModels'); 
-
-module.exports = {
-  async insertInTable(table, data) {
-    const insert = await dbModels.insert(table, data);
-    return {data};
-  }
-}

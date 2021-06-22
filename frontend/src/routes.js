e41402c2f6736c422040a56d2a3f6eb1c5f31399
Nidsonr1 <nidsoncampos@hotmail.com>
2021-06-21 21:05:46 -0300

    Rotas do front

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home/home';
+import Produtos from './pages/Produtos/produtos';
+import Contato from './pages/Contato/contato';
+import Empresa from './pages/Empresa/empresa'
 
 export default function Routes() {
   return(
     <BrowserRouter>
     <Switch>
-       <Route path="/" exact component={Home} />
+      <Route path="/" exact component={Home} />
+      <Route path="/produtos" component={Produtos}/>
+      <Route path="/empresa" component={Empresa}/>
+      <Route path="/contato" component={Contato}/> 
     </Switch>
   </BrowserRouter>
   );

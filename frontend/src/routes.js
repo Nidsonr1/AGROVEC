ad9b439c59d669a6fb20e07c8206bf2516129dc7
Nidsonr1 <nidsoncampos@hotmail.com>
2021-07-06 00:41:52 -0300

    Rotas de Login e Cadastro pelo Banco

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,7 +3,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/home';
 import Produtos from './pages/Produtos/produtos';
 import Contato from './pages/Contato/contato';
-import Empresa from './pages/Empresa/empresa'
+import Empresa from './pages/Empresa/empresa';
+import Login from './pages/Usuario/login';
+import Register from './pages/Usuario/register'
 
 export default function Routes() {
   return(
@@ -13,6 +15,8 @@ export default function Routes() {
       <Route path="/produtos" component={Produtos}/>
       <Route path="/empresa" component={Empresa}/>
       <Route path="/contato" component={Contato}/> 
+      <Route path="/login" component={Login}/>
+      <Route path="/register" component={Register}/>  
     </Switch>
   </BrowserRouter>
   );
